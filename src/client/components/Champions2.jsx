563abdee1255849909e1733ec0ce7853f33ebb2a
johannbesas <johannbesas@gmail.com>
2021-04-13 23:26:30 +1200

    clicking on a champ removes it from the champion list

--- src/client/components/Champions2.jsx
+++ src/client/components/Champions2.jsx
@@ -10,129 +10,158 @@ function Champions2() {
         </>
     )
 
-    function handleClick(event) {
+    
+    const [champions, setList] = useState(championsList)
+
+
+
+    function reverseClick(event) { 
+        
         const redBans = document.getElementById('redBans')
         const redPicks = document.getElementById('redPicks')
         const blueBans = document.getElementById('blueBans')
         const bluePicks = document.getElementById('bluePicks')
+        const champions = document.getElementById('champions')
+        // redBans.removeChild(event.target)
+        console.log('hi')
+    }
 
+    function handleClick(event) {
+    
+        const redBans = document.getElementById('redBans')
+        const redPicks = document.getElementById('redPicks')
+        const blueBans = document.getElementById('blueBans')
+        const bluePicks = document.getElementById('bluePicks')
+        const champions = document.getElementById('champions')
 
-      
         const pic = document.createElement('img')
         pic.setAttribute('src', event.target.src)
+        pic.onclick = reverseClick
 
         if(redBans.childNodes.length === 0)
         {
+            champions.removeChild(event.target)
             redBans.appendChild(pic)
         }
         else if (blueBans.childNodes.length === 0)
         {
+            champions.removeChild(event.target)
             blueBans.appendChild(pic)
         }
         else if (redBans.childNodes.length === 1)
         {
+            champions.removeChild(event.target)
             redBans.appendChild(pic)
         }
         else if (blueBans.childNodes.length === 1)
         {
+            champions.removeChild(event.target)
             blueBans.appendChild(pic)
         }
         else if (redBans.childNodes.length === 2)
         {
+            champions.removeChild(event.target)
             redBans.appendChild(pic)
         }
         else if (blueBans.childNodes.length === 2)
         {
+            champions.removeChild(event.target)
             blueBans.appendChild(pic)
         }
         else if (redPicks.childNodes.length === 0)
         {
+            champions.removeChild(event.target)
             redPicks.appendChild(pic)
         }
         else if (bluePicks.childNodes.length === 0)
         {
+            champions.removeChild(event.target)
             bluePicks.appendChild(pic)
         }
         else if (redPicks.childNodes.length === 1)
         {
+            champions.removeChild(event.target)
             redPicks.appendChild(pic)
         }
         else if (bluePicks.childNodes.length === 1)
         {
+            champions.removeChild(event.target)
             bluePicks.appendChild(pic)
         }
         else if (redPicks.childNodes.length === 2)
         {
+            champions.removeChild(event.target)
             redPicks.appendChild(pic)
         }
         else if (bluePicks.childNodes.length === 2)
         {
+            champions.removeChild(event.target)
             bluePicks.appendChild(pic)
         }
         else if (redBans.childNodes.length === 3)
         {
+            champions.removeChild(event.target)
             redBans.appendChild(pic)
         }
         else if (blueBans.childNodes.length === 3)
         {
+            champions.removeChild(event.target)
             blueBans.appendChild(pic)
         }
         else if (redBans.childNodes.length === 4)
         {
+            champions.removeChild(event.target)
             redBans.appendChild(pic)
         }
         else if (blueBans.childNodes.length === 4)
         {
+            champions.removeChild(event.target)
             blueBans.appendChild(pic)
         }
         else if (redPicks.childNodes.length === 3)
         {
+            champions.removeChild(event.target)
             redPicks.appendChild(pic)
         }
         else if (bluePicks.childNodes.length === 3)
         {
+            champions.removeChild(event.target)
             bluePicks.appendChild(pic)
         }
         else if (redPicks.childNodes.length === 4)
         {
+            champions.removeChild(event.target)
             redPicks.appendChild(pic)
         }
         else if (bluePicks.childNodes.length === 4)
         {
+            champions.removeChild(event.target)
             bluePicks.appendChild(pic)
         }
-        
-        
-
-
-
-
-
     }
-
-    const [champions, setList] = useState(championsList)
 
     return <>
         <div className='flex-auto w-3/12'>
-            <h1 className='pt-2 pb-2 pl-10 bg-red-600 text-white
+            <div className='pt-2 pb-2 pl-10 bg-red-600 text-white
                             xl:text-3xl
                             lg:text-xl
                             md:text-md
-                            sm:text-sm'> Red Side  </h1>
+                            sm:text-sm'> Red Side
+            <input type='text' className = 'ml-10 text-black' />         
+                            </div>
             <h1 className='pt-2 pb-2 pl-10 bg-red-500 text-white
                             xl:text-3xl
                             lg:text-xl
                             md:text-md
                             sm:text-sm'> Bans  </h1>
-            <div className = 'flex flex-wrap' id = "redBans">
-
+            <div className = 'flex flex-wrap h-32' id = "redBans">
             </div>
             <h1 className='pt-2 pb-2 pl-10 bg-red-500 text-white
                             xl:text-3xl
                             lg:text-xl
                             md:text-md
                             sm:text-sm'> Picks  </h1>
-            <div className = 'flex flex-wrap'  id = "redPicks">
+            <div className = 'flex flex-wrap h-32'  id = "redPicks">
 
             </div>
         </div>
@@ -142,24 +171,24 @@ function Champions2() {
                             lg:text-xl
                             md:text-md
                             sm:text-sm'> Champions </h1>
-            <div className='flex flex-auto flex-wrap justify-center'>
-
+            <div id ='champions' className='flex flex-auto flex-wrap justify-center'>
                 {champions}
-
             </div>
         </div>
         <div className='flex-auto w-3/12'>
-            <h1 className='pt-2 pb-2 pl-10 bg-blue-600 text-white
+            <div className='pt-2 pb-2 pl-10 bg-blue-600 text-white
                             xl:text-3xl
                             lg:text-xl
                             md:text-md
-                            sm:text-sm'> Blue Side  </h1>
+                            sm:text-sm'> Blue Side
+            <input type='text' className = 'ml-10 text-black' />         
+                            </div>
             <h1 className='pt-2 pb-2 pl-10 bg-blue-500 text-white
                             xl:text-3xl
                             lg:text-xl
                             md:text-md
                             sm:text-sm'> Bans  </h1>
-            <div className = 'flex flex-wrap' id="blueBans">
+            <div className = 'flex flex-wrap h-32' id="blueBans">
 
             </div>
             <h1 className='pt-2 pb-2 pl-10 bg-blue-500 text-white
@@ -167,7 +196,7 @@ function Champions2() {
                             lg:text-xl
                             md:text-md
                             sm:text-sm'> Picks  </h1>
-            <div className = 'flex flex-wrap' id="bluePicks">
+            <div className = 'flex flex-wrap h-32' id="bluePicks">
 
             </div>
         </div>
